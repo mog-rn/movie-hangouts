de5338e55b02715379ffb5719fb71b8c7d1f29e4
Mogakamo <mogaka.amo254@gmail.com>
2022-09-27 16:26:13 +0300

    [setup]: TypeScript is configured

---
+++ src/screens/Homepage.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native"
+
+export default function Homepage() {
+    return (
+        <View>
+            <Text>This is the Homepage</Text>
+        </View>
+    )
+}
