05526a92f9f18c63ff20174b8873da11ae64282e
Mogakamo <mogaka.amo254@gmail.com>
2022-09-29 15:10:15 +0300

    [navigation]: completed the navigation configuration for the tapp

---
+++ src/constants/ScreenNames.ts
@@ -0,0 +1,14 @@
+// Screen Names
+export enum screen_names {
+    HOME = 'Home',
+    LOGIN = 'LoginScreen',
+    REGISTER = 'RegisterScreen',
+    FORGOT_PASSWORD = 'ForgotPasswordScreen',
+    RESET_PASSWORD = 'ResetPasswordScreen',
+    PROFILE = 'Profile',
+    SETTINGS = 'SettingsScreen',
+    ABOUT = 'AboutScreen',
+    LIST = 'List',
+    SEARCH = 'Search',
+    HOMETABS = 'BottomTabNavigator',
+}
--- src/navigation/MainStackNavigator.tsx
+++ src/navigation/MainStackNavigator.tsx
@@ -1,18 +1,24 @@
 // imports
-import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Homepage from '../screens/Homepage';
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Homepage from "../screens/HomeScreen";
+import { screen_names } from "../constants/ScreenNames";
+import BottomTabsNavigator from "./BottomTabsNavigator";
 
 // Stack Navigator for the main app
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 
 const MainStackNavigator = () => {
-    return (
-        <Stack.Navigator>
-            <Stack.Screen name="Home" component={Homepage} />
-        </Stack.Navigator>
-    )
-}
+  return (
+    <Stack.Navigator>
+      {/* <Stack.Screen name="Home" component={Homepage} /> */}
+      <Stack.Screen
+        name={screen_names.HOMETABS}
+        component={BottomTabsNavigator}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+};
 
-
-export default MainStackNavigator
+export default MainStackNavigator;
