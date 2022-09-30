644557341d3cd4a4bf4c2f85d6406744ef18f9d7
Mogakamo <mogaka.amo254@gmail.com>
2022-09-30 15:15:58 +0300

    [feat]: added google maps to the project and the map is in view

--- src/client/screens/SearchScreen.tsx
+++ src/client/screens/SearchScreen.tsx
@@ -1,12 +1,31 @@
-import { View, Text } from 'react-native'
+import { StyleSheet, View, Text, Dimensions, TextInput } from 'react-native'
 import React from 'react'
+
+// Maps
+import MapView from 'react-native-maps'
+
 
 const Search = () => {
   return (
-    <View>
-      <Text>Search</Text>
+    <View className='relative'>
+      <TextInput className='bg-white absolute inset-1 h-12' />
+      <MapView style={styles.map} />
     </View>
   )
 }
 
 export default Search
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  }
+})
