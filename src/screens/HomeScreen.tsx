5f85a451c7ef813461b280ce3d3580d04766018c
Mogakamo <mogaka.amo254@gmail.com>
2022-09-30 05:38:02 +0300

    [server]: setting up the app's api

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -2,7 +2,7 @@ import { View, Text } from "react-native"
 
 export default function Homepage() {
     return (
-        <View>
+        <View className="flex items-center justify-center h-full">
             <Text>This is the Homepage</Text>
         </View>
     )
