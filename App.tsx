17d04ef6b83b8c1d9a44c545f70b3442faa0a971
Mogakamo <mogaka.amo254@gmail.com>
2022-09-30 06:12:30 +0300

    [config]: project structure and setting the backend API

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import MainStackNavigator from './src/navigation/MainStackNavigator';
+import MainStackNavigator from './src/client/navigation/MainStackNavigator';
 
 
 export default function App() {
