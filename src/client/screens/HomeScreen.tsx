aed8c38c599add45245f7ff8dbbf1ac6ebb6bfbe
Mogakamo <mogaka.amo254@gmail.com>
2022-10-03 05:36:35 +0300

    [feat]: Added the FAB

--- src/client/screens/HomeScreen.tsx
+++ src/client/screens/HomeScreen.tsx
@@ -5,30 +5,53 @@ import {
   Image,
   TouchableOpacity,
   FlatList,
-  ActivityIndicator
-  // NativeBaseProvider,
+  ActivityIndicator,
 } from "react-native";
 import MainLayout from "../layouts/MainLayout";
 import profile from "../assets/profile.jpg";
-
-import { MagnifyingGlassIcon } from "react-native-heroicons/outline";
+import { FloatingAction } from "react-native-floating-action";
+import {
+  MagnifyingGlassIcon,
+  TicketIcon,
+} from "react-native-heroicons/outline";
 import { useEffect, useState } from "react";
 import axios from "axios";
+
+const actions = [
+  {
+    text: "Book a Ticket",
+    name: "bt_ticket",
+    color: "#3A1A6A",
+    icon: require('../assets/icons/ticket.svg')
+  },
+  {
+    text: "Nearby cinemas",
+    name: "bt_cinemas",
+    position: 1,
+    color: "#3A1A6A",
+  },
+  {
+    text: "My Tickets",
+    name: "bt_tickets",
+    position: 1,
+    color: "#3A1A6A",
+  },
+];
 
 export default function Homepage() {
   const [isLoading, setIsLoading] = useState(false);
   const [data, setData] = useState([]);
 
   const upcomingMovies = async () => {
-     return fetch('https://api.themoviedb.org/3/movie/upcoming?api_key=57f69e0d07d803f48a501b9447c516e1&language=en-US&page=1', {
-        method: 'GET',
+    return fetch(
+      "https://api.themoviedb.org/3/movie/upcoming?api_key=57f69e0d07d803f48a501b9447c516e1&language=en-US&page=1",
+      {
+        method: "GET",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
-      })
-      .then((res) => res.json())
-           
-    
+      }
+    ).then((res) => res.json());
   };
 
   useEffect(() => {
@@ -37,7 +60,6 @@ export default function Homepage() {
 
   return (
     <MainLayout>
-      {/* <NativeBaseProvider> */}
       <View className="space-y-5">
         <View className="flex-row items-center justify-between">
           <View className="flex-row items-center">
@@ -79,24 +101,36 @@ export default function Homepage() {
         </View>
       </View>
 
-      {isLoading ? <ActivityIndicator /> : (
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
         <FlatList
           data={data}
-          keyExtractor={(item) => item.id}
+          keyExtractor={(item) => item}
           renderItem={({ item }) => (
             <View className="flex-row items-center justify-between p-3 bg-[#3A1A6A]/40 rounded-xl mb-3">
               <View className="flex-row items-center">
                 <Image
                   source={{
-                    uri: `https://image.tmdb.org/t/p/w500${item.poster_path}`,
+                    uri: `https://image.tmdb.org/t/p/w500${item}`,
                   }}
                   className="w-16 h-16 rounded-xl"
-                  />
-                  </View>
-                  </View>
+                />
+              </View>
+            </View>
           )}
         />
       )}
+      <View className="">
+        <FloatingAction
+          color="#3A1A6A"
+          overlayColor="rgba(68, 68, 68, 0.6)"
+          actions={actions}
+          position="right"
+          distanceToEdge={5}
+          showBackground={false}
+        />
+      </View>
     </MainLayout>
   );
 }
