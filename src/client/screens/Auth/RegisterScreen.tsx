3bce75d846dfc17809c4c68451b16e393daf4406
KoskeiIan <ian.k.koskei@gmail.com>
2022-10-19 15:59:55 +0300

    updates

--- src/client/screens/Auth/RegisterScreen.tsx
+++ src/client/screens/Auth/RegisterScreen.tsx
@@ -1,10 +1,22 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import {Button, TextInput } from 'react-native'
 
 const RegisterScreen = () => {
+  // const [useremail, setUseremail]
+  // const [userpassword, setUserPassword] 
+
+
   return (
-    <View>
-      <Text>RegisterScreen</Text>
+    <View className='p-5'>
+      
+      <Text className='mb-5'>RegisterScreen</Text>
+      <TextInput
+        className='border-2'
+        
+      />
+      <Text>Password</Text>
+      <TextInput
     </View>
   )
 }
