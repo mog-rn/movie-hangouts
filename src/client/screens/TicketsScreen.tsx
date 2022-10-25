bc84a61c99d33f09fe5e99c4cbc78429fd427639
Liz Odhiambo <elizabethodhiambo418@gmail.com>
2022-10-25 16:41:08 +0300

    updated tickets creen

--- src/client/screens/TicketsScreen.tsx
+++ src/client/screens/TicketsScreen.tsx
@@ -1,11 +1,24 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextInput, Button } from 'react-native'
 import React from 'react'
-import MainLayout from '../layouts/MainLayout'
+import MainLayout f  c rom '../layouts/MainLayout'
 
 const Tickets = () => {
   return (
     <MainLayout>
-      <Text>Tickets</Text>
+      <Text className='text-white font-bold text-xl'>Book your Ticket</Text>
+      <View className='py-4 space-y-4'>
+        <Text className='text-white'>Name</Text>
+        <TextInput className='border-white border-2 text-white placeholder:text-yellow focus:text-white h-12 p-4' placeholder='Please add your name' />
+        <Text className='text-white'>Movie</Text>
+        <TextInput className='border-white border-2 text-[16px] text-white placeholder:h-12 p-3' placeholder='Enter name of movie'/>
+        <Text className='text-white' > Time </Text>
+        <TextInput />
+        <Text className='text-white'>Price</Text>
+        <TextInput placeholder= 'price' />
+      </View>
+
+      <Button color="#113126" title="Book Now!" />
+
     </MainLayout>
   )
 }
