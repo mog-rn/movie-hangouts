a02b5870eca68992bf23d8376542a7549c40ce2f
Liz Odhiambo <elizabethodhiambo418@gmail.com>
2022-10-27 16:23:29 +0300

    Latest changes

--- src/client/screens/TicketsScreen.tsx
+++ src/client/screens/TicketsScreen.tsx
@@ -1,23 +1,25 @@
 import { View, Text, TextInput, Button } from 'react-native'
 import React from 'react'
-import MainLayout f  c rom '../layouts/MainLayout'
+import MainLayout from '../layouts/MainLayout'
 
 const Tickets = () => {
   return (
     <MainLayout>
       <Text className='text-white font-bold text-xl'>Book your Ticket</Text>
-      <View className='py-4 space-y-4'>
+      <View className='py-5 space-y-4'>
         <Text className='text-white'>Name</Text>
-        <TextInput className='border-white border-2 text-white placeholder:text-yellow focus:text-white h-12 p-4' placeholder='Please add your name' />
+        <TextInput className='rounded-md border-white border-2 text-white placeholder:text-yellow focus:text-white h-12 p-3' placeholder='Please add your name' />
         <Text className='text-white'>Movie</Text>
-        <TextInput className='border-white border-2 text-[16px] text-white placeholder:h-12 p-3' placeholder='Enter name of movie'/>
+        <TextInput className='rounded-md border-white border-2 text-[16px] text-white placeholder:h-12 p-3' placeholder='Enter name of movie'/>
         <Text className='text-white' > Time </Text>
-        <TextInput />
+        <TextInput className= 'rounded-md border-white border-2 text-white placeholder:h-12 p-3' placeholder='Enter time of movie' />
         <Text className='text-white'>Price</Text>
-        <TextInput placeholder= 'price' />
+        <TextInput className='rounded-md border-white border-2 text-white placeholder:h-12 p-3' placeholder= 'price of movie' />
       </View>
 
-      <Button color="#113126" title="Book Now!" />
+      <Button onPress={() => {
+    alert('Book!');
+  }} color="#113126" title="Book Now!" />
 
     </MainLayout>
   )
